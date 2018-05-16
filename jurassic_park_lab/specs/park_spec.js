f8f0fe68c7f6c70e03630ada36c1d4d670ebfc40
Joseph Manuel <mickeyjoe75@gmail.com>
2018-05-16 01:15:59 +0100

    updated files

---
+++ jurassic_park_lab/specs/park_spec.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  beforeEach(function () {
+
+  })
+
+  xit('should have a name', function () {
+
+  });
+
+  xit('should have a ticket price', function () {
+
+  });
+
+  xit('should have a collection of dinosaurs', function () {
+
+  });
+
+  xit('should be able to add a dinosaur to its collection', function () {
+
+  });
+
+  xit('should be able to remove a dinosaur from its collection', function () {
+
+  });
+
+  xit('should be able to find all dinosaurs of a particular species', function () {
+
+  });
+
+  xit('should be able to remove all dinosaurs of a particular species', function () {
+
+  });
+
+  xit('should be able to find the dinosaur that attracts the most visitors', function () {
+
+  });
+
+});
