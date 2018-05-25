b8b97b6c1d65d234397be73f33055315fb5a4262
Joseph Manuel <mickeyjoe75@gmail.com>
2018-05-25 03:37:47 +0100

    code academy project whale talk corrected typo,final code

--- whaleTalk.js
+++ whaleTalk.js
@@ -3,7 +3,7 @@ var vowels = ['a','e','i','o','u'];
 var resultArray = [];
 
 for (var i = 0; i < input.length; i++){
-  for(var j = 0; j< vowels.lenth; j++){
+  for(var j = 0; j< vowels.length; j++){
     if (input[i] === vowels[j]) {
       resultArray.push(input[i]);
     }
