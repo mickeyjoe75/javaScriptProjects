c42ee8af0932ec59095f655b928432de4320b24d
Joseph Manuel <mickeyjoe75@gmail.com>
2018-05-25 21:39:43 +0100

    code academy projects passowrd validator js projectcompleted and tested

--- codeAcademy_projects/passwordValidator.js
+++ codeAcademy_projects/passwordValidator.js
@@ -1,3 +1,22 @@
+
+function isPasswordValid(input) {
+  if (hasUpperCase(input) && hasLowerCase(input) && isLongEnough(input) && hasSpecialCharacter(input)) {
+    console.log('Password is valid');
+  }
+  if (!hasUpperCase(input)) {
+    console.log('Invalid Password! Password needs a capital letter');
+  }
+  if (!hasLowerCase(input)) {
+    console.log('Invalid Password!Password needs a lowercase letter');
+  }
+  if (!isLongEnough(input)) {
+    console.log('Invalid Password! Password needs to be atleast 8 characters');
+  }
+  if (!hasSpecialCharacter(input)){
+    console.log('Invalid Password! Password should contain at least a specialCharacter!');
+  }
+}
+
 function hasUpperCase(input) {
   for (var i = 0; i < input.length; i++ ) {
     if (input[i] === input[i].toUpperCase()) {
@@ -6,11 +25,30 @@ function hasUpperCase(input) {
   }
 }
 
-
-function isPasswordValid(input) {
-  if (hasUpperCase(input)) {
-    console.log('Password is valid');
+function hasLowerCase(input) {
+  for( var i in input){
+    if(input[i] === input[i].toLowerCase()){
+      return true;
+    }
   }
 }
 
-isPasswordValid('Kundi');
+function isLongEnough(input) {
+  if (input.length >= 8) {
+    return true;
+  }
+}
+
+function hasSpecialCharacter(input) {
+  var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*'];
+  for (var i in input) {
+    for (var character in specialCharacters) {
+      if (input[i] === specialCharacters[character]) {
+        return true;
+      }
+    }
+  }
+}
+
+
+isPasswordValid('KK999iii@');
