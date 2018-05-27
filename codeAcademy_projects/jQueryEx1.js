5348ee210efe1529e1934f3c2c34d201199778b9
Joseph Manuel <mickeyjoe75@gmail.com>
2018-05-27 18:10:06 +0100

    codeacademy project jquery example and litbrite added

---
+++ codeAcademy_projects/jQueryEx1.js
@@ -0,0 +1,17 @@
+/*var skillset = document.getElementsByClassName('skillset');
+alert(skillset);
+*/
+function main(){
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click', function() {
+   //$(this).next().toggle();
+   $(this).next().slideToggle(400);
+   $(this).toggleClass('active');
+   $(this).text('Projects Viewed');
+
+  });
+
+}
+$(document).ready(main);
